049cc1211bf35fe643e347e42dfe08c676fa0069
lethanner <50079644+lethanner@users.noreply.github.com>
2023-11-14 14:16:15 +0400

    фикс проблем с дозвоном (ложное поднятие трубки)

--- src/config.h
+++ src/config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define NUM_LENGTH 6
-#define ANSWER_WAIT_MS 10000
-#define DIAL_WAIT_MS 10000
+#define ANSWER_WAIT_MS 180000 // 180с ожидания ответа по стандарту
+#define DIAL_WAIT_MS 30000    // 30с - придумано от балды
 
 #define LINE_READY_OR_BUSY 425
 #define OFF_HOOK_WARNING 950
@@ -17,4 +17,4 @@
 #define LINE2_RING 5
 #define LINE_CONNECT 6
 
-#define OFF_HOOK_ADC 750
+#define OFF_HOOK_ADC 800
